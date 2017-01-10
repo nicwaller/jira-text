014e7db0af5a94dd8a9d44ff75ce5afdb103fbed
Nic Waller <nic.waller@pulseenergy.com>
2017-01-10 00:59:53 -0800

    fix console entry point

--- setup.py
+++ setup.py
@@ -19,7 +19,7 @@ setup(
     ],
     entry_points={
         "console_scripts": [
-            "jiraexport=jiraexport.jiraexport:main",
+            "jiraexport=jiraexport.cli:main",
         ]
     },
     packages=find_packages(),
